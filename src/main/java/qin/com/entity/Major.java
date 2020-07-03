5ca7a88b8e9e2f0216eeaa64357f948b908f2b0a
刘翔 <lx982265791@163.com>
2020-07-03 20:40:09 +0800

    major

---
+++ src/main/java/qin/com/entity/Major.java
@@ -0,0 +1,45 @@
+package qin.com.entity;
+
+import java.util.Date;
+
+public class Major {
+    private Integer id;
+
+    private String name;
+
+    private Date createTime;
+
+    private String updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime == null ? null : updateTime.trim();
+    }
+}
